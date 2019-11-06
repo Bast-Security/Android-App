5977cf6faf227d7933884d2fc1ac81f44183be6f
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2019-11-05 20:33:44 -0800

    Added XML and Kotlin files

---
+++ app/src/main/java/com/example/bast/CreateSystem.kt
@@ -0,0 +1,12 @@
+package com.example.bast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateSystem : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_system)
+    }
+}
