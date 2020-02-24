090e2a05baaf310440e023f2172a44d9de7ee0f6
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2020-02-23 22:50:12 -0800

    changed activities

--- app/src/main/java/com/example/bast/ConnectSystemActivity.java
+++ app/src/main/java/com/example/bast/ConnectSystemActivity.java
@@ -1,6 +1,9 @@
 package com.example.bast;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,10 +13,17 @@ public class ConnectSystemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_system);
 
-        setContentView(R.layout.activity_system_menu);
-
-
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText systemName = (EditText) findViewById(R.id.username);
+                String name = systemName.toString();
+                setContentView(R.layout.success_notif);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/bast/SystemMenuActivity.java
+++ app/src/main/java/com/example/bast/SystemMenuActivity.java
@@ -1,10 +1,7 @@
 package com.example.bast;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,17 +12,7 @@ public class SystemMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_system_menu);
 
-        setContentView(R.layout.activity_connect_system);
-
-
-        Button button= (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText systemName = (EditText) findViewById(R.id.username);
-                String name = systemName.toString();
-                setContentView(R.layout.success_notif);
-            }
-        });
     }
 }
