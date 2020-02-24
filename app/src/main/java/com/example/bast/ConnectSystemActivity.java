6beaf1467956feb5cd97ce8f746eae0a8bdd0915
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2020-02-23 23:36:25 -0800

    made dialog for success notification

--- app/src/main/java/com/example/bast/ConnectSystemActivity.java
+++ app/src/main/java/com/example/bast/ConnectSystemActivity.java
@@ -1,14 +1,21 @@
 package com.example.bast;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class ConnectSystemActivity extends AppCompatActivity {
+
+    Dialog successDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +23,30 @@ public class ConnectSystemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_connect_system);
 
         Button button = (Button) findViewById(R.id.button);
+        EditText systemName = (EditText) findViewById(R.id.username);
+
+        String name = systemName.toString();
+
+        successDialog = new Dialog(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText systemName = (EditText) findViewById(R.id.username);
-                String name = systemName.toString();
-                setContentView(R.layout.success_notif);
+                openDialog();
+                //setContentView(R.layout.success_notif);
             }
         });
 
+    }
+
+    public void openDialog() {
+        successDialog.setContentView(R.layout.success_notif);
+        ImageView check = (ImageView) successDialog.findViewById(R.id.success_check);
+        TextView successText = (TextView) successDialog.findViewById(R.id.success_text);
+
+        successDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        successDialog.show();
+
+        successDialog.setCanceledOnTouchOutside(true);
     }
 
 }
