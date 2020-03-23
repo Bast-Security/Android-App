f410add40fc4dae852d7c9f1cc2c5c1543a643bb
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2020-03-23 00:16:01 -0700

    implemented swipe LEFT to delete

--- app/src/main/java/com/example/bast/RoleListActivity.java
+++ app/src/main/java/com/example/bast/RoleListActivity.java
@@ -1,6 +1,7 @@
 package com.example.bast;
 
 import android.app.Dialog;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -9,22 +10,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bast.list_adapters.RolesAdapter;
+import com.example.bast.objects.Lock;
 import com.example.bast.objects.Role;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
 public class RoleListActivity extends AppCompatActivity {
 
     public ArrayList<Role> roles = new ArrayList<>();
+    RecyclerView rv;
+    RolesAdapter adapter;
     Dialog addDialog;
 
     @Override
@@ -71,10 +81,13 @@ public class RoleListActivity extends AppCompatActivity {
 
     // initializes the view of roles
     private void rolesRecyclerView() {
-        RecyclerView rv = findViewById(R.id.recycler_view);
-        RolesAdapter adapter = new RolesAdapter(this, roles);
+        rv = findViewById(R.id.recycler_view);
+        adapter = new RolesAdapter(this, roles);
         rv.setAdapter(adapter);
         rv.setLayoutManager(new LinearLayoutManager(this));
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(rv);
     }
 
     private void initButton() {
@@ -110,4 +123,40 @@ public class RoleListActivity extends AppCompatActivity {
         addDialog.show();
 
     }
+
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int pos = viewHolder.getAdapterPosition();
+            Role deletedRole = roles.remove(pos);
+            adapter.notifyItemRemoved(pos);
+
+            Snackbar.make(rv, deletedRole.getRoleName(), Snackbar.LENGTH_LONG)
+                    .setAction("Undo", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            roles.add(pos, deletedRole);
+                            adapter.notifyItemInserted(pos);
+                        }
+                    }).show();
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView rv, @NonNull RecyclerView.ViewHolder viewHolder,
+                                float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            new RecyclerViewSwipeDecorator.Builder(c, rv, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addSwipeLeftBackgroundColor(ContextCompat.getColor(RoleListActivity.this, R.color.delete))
+                    .addActionIcon(R.drawable.ic_delete_black_24dp)
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, rv, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 }
