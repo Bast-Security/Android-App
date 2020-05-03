7b4d8897ebea3e513d4ca22223f5ed225c8b3f63
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2020-05-02 20:11:44 -0700

    name change and added incomplete function

--- app/src/main/java/com/example/bast/AddLockActivity.java
+++ app/src/main/java/com/example/bast/AddLockActivity.java
@@ -36,7 +36,7 @@ public class AddLockActivity extends AppCompatActivity {
         session = new Session(jwt);
 
         final TextView code = findViewById(R.id.code);
-        final TextView conuntdownTimer = findViewById(R.id.countdown_timer);
+        final TextView conuntdownTimer = findViewById(R.id.timer);
 
         setContentView(R.layout.activity_connect_lock);
 
@@ -59,4 +59,10 @@ public class AddLockActivity extends AppCompatActivity {
             }
         });
     }
+
+    // timer for code refresh
+    public void countdown(){
+
+    }
+
 }
