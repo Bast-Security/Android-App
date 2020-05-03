6fd0a66c1f3060bb37068fc6ef468fcbd9d8c811
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2020-05-02 19:25:37 -0700

    created activity for AddLock

--- app/src/main/java/com/example/bast/AddLockActivity.java
+++ app/src/main/java/com/example/bast/AddLockActivity.java
@@ -1,4 +1,17 @@
 package com.example.bast;
 
-public class AddLockActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddLockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_lock);
+
+
+    }
+
 }
