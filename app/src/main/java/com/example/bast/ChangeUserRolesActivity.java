465895ea17bbf16928439741a644ae7d9c2dead1
evanpascual <32683638+evanpascual@users.noreply.github.com>
2020-05-09 20:11:49 -0700

    change user roles

--- app/src/main/java/com/example/bast/ChangeUserRolesActivity.java
+++ app/src/main/java/com/example/bast/ChangeUserRolesActivity.java
@@ -1,40 +1,86 @@
 package com.example.bast;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bast.list_adapters.UserRoleCheckList;
+import com.example.bast.objects.Async;
+import com.example.bast.objects.HTTP;
 import com.example.bast.objects.Role;
 import com.example.bast.objects.Session;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Response;
 
 public class ChangeUserRolesActivity extends AppCompatActivity {
 
     private Session session;
     private ListView lv;
     private UserRoleCheckList adapter;
-    private List<Role> roles = new ArrayList<>();
+    private List<Role> rolesList = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_roles);
-        //getRoles();
-        roles.add(0, new Role("President"));
-        roles.add(new Role("Janitor"));
+
+        final Bundle bundle = getIntent().getExtras();
+        final String jwt = bundle.getString("jwt");
+        session = new Session(jwt);
+        getRoles(session);
+
 
         lv = findViewById(R.id.checkbox_list);
-        adapter = new UserRoleCheckList(roles, this);
+        adapter = new UserRoleCheckList(rolesList, this);
         lv.setAdapter(adapter);
 
     }
 
-    private void getRoles() {
+    private void getRoles(Session session) {
         // TODO: fill in the roles from the database
+        rolesList.add(0, new Role("President"));
+        rolesList.add(new Role("Janitor"));
+
+
+        final Handler handler = new Handler();
+        final Bundle bundle = getIntent().getExtras();
+        final int systemId = bundle.getInt("systemId");
+        Async.task(() -> {
+            String HTTPGet = "systems/" + systemId + "/roles";
+            try (final Response response = session.request(HTTP.get(HTTPGet))) {
+                if (response.isSuccessful()) {
+                    final String responseBody = response.body().string();
+                    Log.d("role", responseBody);
+                    rolesList.removeAll(rolesList);
+
+                    final JSONArray roles = new JSONArray(responseBody);
+                    for (int i = 0; i < roles.length(); i++) {
+                        final JSONObject object = roles.getJSONObject(i);
+                        final Role role = new Role(object.getString("name"));
+                        rolesList.add(role);
+                    }
+
+                    handler.post(() -> adapter.notifyDataSetChanged());
+                } else {
+                    Log.d("role", "Bad response from server");
+                }
+            } catch (IOException e) {
+                Log.d("role", "Failed to connect to host " + e.toString());
+            } catch (JSONException e) {
+                Log.d("role", "Failed to parse JSON string " + e.toString());
+            }
+        });
     }
 }
