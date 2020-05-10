0287409e8ca6d588bcb71ae4bd54d23775b872d3
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2020-05-09 18:31:35 -0700

    added integer correlation for dropdown settings

--- app/src/main/java/com/example/bast/EditLockActivity.java
+++ app/src/main/java/com/example/bast/EditLockActivity.java
@@ -32,6 +32,7 @@ public class EditLockActivity extends AppCompatActivity {
         categories.add(0, "Choose Mode");
         categories.add("Pin Only");
         categories.add("Card Only");
+        categories.add("Card or Pin");
         categories.add("Pin and Card");
 
         ArrayAdapter<String> dataAdapter;
@@ -42,13 +43,31 @@ public class EditLockActivity extends AppCompatActivity {
         dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int choice;
 
                 if(parent.getItemAtPosition(position).equals("Choose Mode")) { }
-                else {
+                else if(parent.getItemAtPosition(position).equals("Pin Only")) {
+                    choice = 4;
                     String item = parent.getItemAtPosition(position).toString();
-
                     Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
-
+                    // TODO: edit changes made into database
+                }
+                else if(parent.getItemAtPosition(position).equals("Card Only")) {
+                    choice = 2;
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
+                    // TODO: edit changes made into database
+                }
+                else if(parent.getItemAtPosition(position).equals("Pin or Card")) {
+                    choice = 6;
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
+                    // TODO: edit changes made into database
+                }
+                else if(parent.getItemAtPosition(position).equals("Pin and Card")) {
+                    choice = 8;
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_SHORT).show();
                     // TODO: edit changes made into database
                 }
             }
